edaefbdaceb67f49800b5b86b73e749881fffdc8
Tushar Tikkiwal <ttikkiwal@gmail.com>
2025-06-17 01:04:18 +0530

    changes

---
+++ models/Chat.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    latestMessage: {
+      type: String,
+      default: "New Chat",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Chat = mongoose.model("Chat", Schema);
