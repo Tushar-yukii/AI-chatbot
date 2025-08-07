f64bc26332ef709beb9fef918af183851a8e9b54
Tushar Tikkiwal <ttikkiwal@gmail.com>
2025-08-07 23:47:10 +0530

    commit

--- middlewares/isAu.js
+++ middlewares/isAu.js
@@ -1,22 +1,22 @@
-// import jwt from "jsonwebtoken";
-// import { User } from "../models/User";
+import jwt from "jsonwebtoken";
+import { User } from "../models/User";
 
-// export const isAuth = async (req, res, next) => {
-//   try {
-//     const token = req.header.token;
+export const isAuth = async (req, res, next) => {
+  try {
+    const token = req.header.token;
 
-//     if (!token)
-//       return res.status(403).json({
-//         message: "please login",
-//       });
-//     const decode = jwt.verify(token, process.env.jwt_sec);
+    if (!token)
+      return res.status(403).json({
+        message: "please login",
+      });
+    const decode = jwt.verify(token, process.env.jwt_sec);
 
-//     req.user = await User.findById(decode._id);
+    req.user = await User.findById(decode._id);
 
-//     next();
-//   } catch (error) {
-//     res.status(500).json({
-//       message: "login First",
-//     });
-//   }
-// };
+    next();
+  } catch (error) {
+    res.status(500).json({
+      message: "login First",
+    });
+  }
+};
